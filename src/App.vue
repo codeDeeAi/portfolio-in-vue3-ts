bf039b6ce6c399e4c283c29a0decb3f91e65764f
Adeola Ephraim <dunsin.bada@gmail.com>
2022-07-26 12:50:18 +0100

    init app, added preloader

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import PreLoader from './components/PreLoader.vue'
+
+const isLoaded = ref<Boolean>(false);
+
+// ## Methods
+/**
+ * Toggles page loader off
+ * @returns {void}
+ */
+const toggleLoader = (): void => {
+  window.onload = function () {
+    return isLoaded.value = true;
+  };
+}
+
+// ## On created
+toggleLoader();
+</script>
+
+<template>
+  <!-- Loader -->
+  <PreLoader v-if="!isLoaded" />
+  <!-- Loader Ends -->
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+import './assets/tailwind.css'
+
+
+const app = createApp(App)
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
